04b4c25a4ead5008b2648634de58b118355952a2
chenmengchieh2012 <chenmengchieh2012@gmail.com>
2017-06-20 00:31:16 +0800

    test another loading

--- js/loading.js
+++ js/loading.js
@@ -1,4 +1,18 @@
-window.onload =  \
-	function(){ 
-		document.getElementById("loading").style.display = "none" 
-	}
+function onReady(callback) {
+    var intervalID = window.setInterval(checkReady, 500);
+    function checkReady() {
+        if (document.getElementsByTagName('body')[0] !== undefined) {
+            window.clearInterval(intervalID);
+            callback.call(this);
+        }
+    }
+}
+
+function show(id, value) {
+    document.getElementById(id).style.display = value ? 'block' : 'none';
+}
+
+onReady(function () {
+    show('page', true);
+    show('loading', false);
+});
